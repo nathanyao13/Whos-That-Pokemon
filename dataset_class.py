4571318c6e3e25e65814a0f0dc13242169484b3e
Nathan Yao <nyao@andrew.cmu.edu>
2025-05-29 09:25:37 -0700

    training completed for all classes, going to change dataset and see results for next step

--- dataset_class.py
+++ dataset_class.py
@@ -3,16 +3,16 @@ from torch.utils.data import Dataset
 from PIL import Image
 
 class PokemonImages(Dataset):
-    def __init__(self, csv_file, label, transform = None):
-        self.csv = pd.read_csv(csv_file)
+    def __init__(self, df, label, transform = None):
+        self.df = df.reset_index(drop=True)
         self.transform = transform
         self.label = label
     
     def __len__(self):
-        return len(self.csv)
+        return len(self.df)
     
     def __getitem__(self,index):
-        img_path = self.csv.loc[index, 'image_path']
+        img_path = self.df.loc[index, 'image_path']
         label = self.df.loc[index,self.label]
 
         image = Image.open(img_path).convert("RGB")
